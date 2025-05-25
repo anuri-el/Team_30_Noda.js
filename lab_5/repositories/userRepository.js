e438dc8bebbe4b55718591796158bd102ab8fae8
rshvtss <r.shvetsss@gmail.com>
2025-05-26 00:57:00 +0300

    almost done w sequalize 4 users
    треба пофіксити логін, реєстрація в новий файл працює

--- lab_5/repositories/userRepository.js
+++ lab_5/repositories/userRepository.js
@@ -1,97 +1,49 @@
-const db = require("../database/database.js");
+const Users = require("../database/models/users.js");
 
 exports.getUserByEmail = async (email) => {
-	user = new Promise((resolve, reject) => {
-		db.get("SELECT * FROM users WHERE email = ?", [email], (err, row) => {
-			if (err) return reject(err);
-			resolve(row);
-		});
-	});
-	console.log("rep", user);
-	return user;
+	try {
+		const user = await Users.findOne({ where: { email } });
+		return user ? user.get({ plain: true }) : null;
+	} catch (err) {
+		throw err;
+	}
 };
 
 exports.getUserById = async (id) => {
-	console.log("get user by id");
-	return new Promise((resolve, reject) => {
-		db.get("SELECT * FROM users WHERE ID = ?", [id], (err, row) => {
-			if (err) return reject(err);
-			resolve(row);
-		});
-	});
+	try {
+		const user = await Users.findByPk(id);
+		return user ? user.get({ plain: true }) : null;
+	} catch (err) {
+		throw err;
+	}
 };
 
-exports.createUser = async (user) => {
-	const { name, email, password } = user;
-
-	return new Promise((resolve, reject) => {
-		db.serialize(() => {
-			db.run("BEGIN TRANSACTION");
-
-			db.run(
-				"INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-				[name, email, password],
-				function (err) {
-					if (err) {
-						db.run("ROLLBACK");
-						return reject(err);
-					}
-
-					const insertedId = this.lastID;
-
-					db.get("SELECT * FROM users WHERE ID = ?", [insertedId], (err, row) => {
-						if (err) {
-							db.run("ROLLBACK");
-							return reject(err);
-						}
-
-						db.run("COMMIT", (err) => {
-							if (err) return reject(err);
-							resolve(row);
-						});
-					});
-				}
-			);
-		});
-	});
+exports.createUser = async ({ name, email, password }) => {
+	try {
+		const user = await Users.create({ name, email, password });
+		return user.get({ plain: true });
+	} catch (err) {
+		throw err;
+	}
 };
 
 exports.updateUserName = async (id, newName) => {
-	return new Promise((resolve, reject) => {
-		db.serialize(() => {
-			db.run("BEGIN TRANSACTION");
-
-			db.run(
-				"UPDATE users SET name = ? WHERE ID = ?",
-				[newName, id],
-				function (err) {
-					if (err) {
-						db.run("ROLLBACK");
-						return reject(err);
-					}
-
-					db.get("SELECT * FROM users WHERE ID = ?", [id], (err, row) => {
-						if (err) {
-							db.run("ROLLBACK");
-							return reject(err);
-						}
-
-						db.run("COMMIT", (err) => {
-							if (err) return reject(err);
-							resolve(row);
-						});
-					});
-				}
-			);
-		});
-	});
+	try {
+		const user = await Users.findByPk(id);
+		if (!user) return null;
+		user.name = newName;
+		await user.save();
+		return user.get({ plain: true });
+	} catch (err) {
+		throw err;
+	}
 };
 
 exports.getAllUsers = async () => {
-	return new Promise((resolve, reject) => {
-		db.all("SELECT * FROM users", (err, rows) => {
-			if (err) return reject(err);
-			resolve(rows);
-		});
-	});
+	try {
+		const users = await Users.findAll();
+		return users.map((user) => user.get({ plain: true }));
+	} catch (err) {
+		throw err;
+	}
 };
